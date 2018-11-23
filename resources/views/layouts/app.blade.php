012de0ac13107c2193edf6f5d0bd8bd95832f8e2
whx <whx@lumokeji.com>
2018-11-23 02:19:33 +0000

    Finish basic page layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="with=device-width, initial-scale=1">
 
     <!-- CSRF TOKEN -->
-    <meta name="csrf-token" content="{{ cstf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS') - Laravel Study</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset(' css/app.css ') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
