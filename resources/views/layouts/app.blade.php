874a7a41d01d64fbb1f187c306bd950c36080708
whx <whx@lumokeji.com>
2018-11-26 09:51:24 +0000

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <title>@yield('title', 'LaraBBS') - Laravel Study</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -24,7 +25,8 @@
     </div>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 
-    <script src=" {{ asset('js/app.js')}} "></script>
 </body>
 </html>
