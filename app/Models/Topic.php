1014bc7de245f0103261c33b3ee0282e9d81648c
whx <whx@lumokeji.com>
2018-11-26 03:05:43 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,15 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 模型关联 topic --> category 一对一
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 模型关联 topic --> user 一对一
+    public function User() {
+        return $this->belongsTo(User::class);
+    }
+
 }
