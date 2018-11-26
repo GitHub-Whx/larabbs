419b916d1d5ac221825b90363c1ce5b325cdba89
whx <whx@lumokeji.com>
2018-11-26 08:24:53 +0000

    User can create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Models\Topic;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
@@ -11,6 +13,7 @@ class TopicsController extends Controller
 {
     public function __construct()
     {
+        // 'except' => ['index', 'show'] —— 对除了 index() 和 show() 以外的方法使用 auth 中间件进行认证
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
@@ -27,19 +30,25 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+    // 参数 TopicRequest $request 注入对应的表单验证类( app/Http/Requests/TopicRequest.php)
+	public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
 
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+         $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
@@ -57,4 +66,5 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
 }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -18,4 +18,11 @@ class TopicObserver
     {
         //
     }
+
+    // 方法名 saving 即为监听的事件
+    public function saving(Topic $topic) {
+        // excerpt 是 topic 模型的一个字段
+        // meke_excerpt 自定义的辅助方法 在 bootsrap/helpers.php
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 }
