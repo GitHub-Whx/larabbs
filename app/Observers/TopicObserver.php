466256a3d884056768fdaac5d80241c1096ee4f4
whx <whx@lumokeji.com>
2018-11-27 02:57:52 +0000

    Auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-
+use App\Handlers\SlugTranslateHandler;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -21,10 +21,15 @@ class TopicObserver
 
     // 方法名 saving 即为监听的事件
     public function saving(Topic $topic) {
-        // html 内容过滤
+        // XSS 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
-        // excerpt 是 topic 模型的一个字段
-        // meke_excerpt 自定义的辅助方法 在 bootsrap/helpers.php
+
+        // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
